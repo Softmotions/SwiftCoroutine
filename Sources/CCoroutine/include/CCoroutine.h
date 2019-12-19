7087192263c5e18ddc20dc319d16d73b23297c1c
Adamansky Anton <adamansky@gmail.com>
2019-12-19 21:15:23 +0700

    Update CCoroutine.h

--- Sources/CCoroutine/include/CCoroutine.h
+++ Sources/CCoroutine/include/CCoroutine.h
@@ -5,11 +5,12 @@
 //  Created by Alex Belozierov on 22.11.2019.
 //  Copyright © 2019 Alex Belozierov. All rights reserved.
 //
+#include <setjmp.h>
 
 #ifndef CCoroutine_h
 #define CCoroutine_h
 
-int __start(int* ret, const void* stack, const void* param, const void (*block)(const void*));
-int __save(int* env, int* ret, int retVal);
+int __start(jmp_buf ret, const void* stack, const void* param, const void (*block)(const void*));
+int __save(jmp_buf env, jmp_buf ret, int retVal);
 
 #endif
